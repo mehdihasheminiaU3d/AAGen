4cff63e306812ee2f93d2986c49481f3afa260da
Joel Barba <joel.barba@unity3d.com>
2025-06-03 19:56:50 -0500

    Documenting Bootloader.cs

--- Runtime/BootLoader.cs
+++ Runtime/BootLoader.cs
@@ -4,12 +4,23 @@ using UnityEngine.SceneManagement;
 
 namespace AAGen.Runtime
 {
+    /// <summary>
+    /// Represents a <see cref="Component"/> that can load a scene at the start of an empty bootstrapper scene.
+    /// </summary>
     public class BootLoader : MonoBehaviour
     {
-        public AssetReference m_SceneToLoad;
+        /// <summary>
+        /// The scene to load.
+        /// </summary>
+        [SerializeField]
+        private AssetReference m_SceneToLoad;
         
-        void Start()
+        /// <summary>
+        /// Called by the Unity system the first time that this component is ready to update itself, according to the time slice. 
+        /// </summary>
+        private void Start()
         {
+            // Closes all currently loaded scenes and loads a single scene asynchronously.
             Addressables.LoadSceneAsync(m_SceneToLoad, LoadSceneMode.Single);
         }
     }
